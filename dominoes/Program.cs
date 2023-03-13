2652735549c4bfc5b420a580e5272125bcee0d75
Alexis Sánchez <126355162+SanchezRuano22211659@users.noreply.github.com>
2023-03-13 19:37:24 +0000

    new file:   dominoes/Program.cs 	new file:   dominoes/dominoes.csproj

---
+++ dominoes/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Domino
+{
+    private int faceUp;
+    private int faceDown;
+        public Domino(int _faceUp,int _faceDown)
+    {
+        this.faceUp = _faceUp;
+        this.faceDown = _faceDown;
+    }
+    public static Domino operator + (Domino a,Domino b)
+    {
+        int den= a.faceDown + b.faceDown;
+        int num= a.faceUp + b.faceUp;
+        return new Domino (num,den);
+    }
+
+    public void Print_()
+    {
+        Console.WriteLine(faceUp+faceDown);
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Domino d1 = new Domino(0,1);
+        Domino d2 = new Domino(2,3);
+        Domino d3 = d1 + d2;
+        d3.Print_();
+    }
+}
